adf073a1e72bd4da1886d3f9a12e267f94764efb
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-11-07 22:01:15 -0800

    WIP: just copy module work

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,45 @@
-const { Model, DataTypes } = require("sequelize");
+const {
+	Sequelize,
+	DataTypes,
+	// Model,
+} = require("sequelize");
 const sequelize = require("../config/connection.js");
+// const sequelize = new Sequelize("sqlite::memory:");
 
 // Do I need to import `Product` to create the One-To-Many relationship? https://sequelize.org/docs/v6/core-concepts/assocs/#one-to-many-relationships
 const Product = require("./Product");
-class Category extends Model {}
+// class Category extends Model {}
 
-console.log(sequelize.models);
+// console.log("sequelize.models:");
+// console.log(sequelize.models);
+// console.log(Product);
+
+const Category = sequelize.define(
+	"Category",
+	{
+		// Model attributes are defined here
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		category_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "category",
+	}
+);
 
 // Category.init(
+/** 
 Category.init(
 	{
 		id: {
@@ -29,8 +61,7 @@ Category.init(
 		modelName: "category",
 	}
 );
+*/
 
-Category.hasOne(Product);
-Product.belongsTo(Category);
+
 // module.exports = Category;
-module.exports = Category;
--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,12 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require("sequelize");
+const {
+	Sequelize,
+	DataTypes,
+	// Model,
+} = require("sequelize");
+
+// const sequelize = new Sequelize("sqlite::memory:");
+
 // import our database connection from config.js
 const sequelize = require("../config/connection");
 
@@ -7,10 +14,58 @@ const sequelize = require("../config/connection");
 const { Category } = require("./Category.js");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+// class Product extends Model {}
 
 // set up fields and rules for Product model
-// Product.init(
+
+const Product = sequelize.define(
+	"Product",
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		product_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.DECIMAL,
+			allowNull: false,
+			validate: {
+				isDecimal: true,
+				msg: "Must be a decimal",
+			},
+		},
+		stock: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 10,
+			validate: {
+				isInt: true,
+				msg: "Must be an integer",
+			},
+		},
+		category_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "Category",
+				key: "id",
+			},
+			// TO DO: references category model's ID
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "product",
+	}
+);
+/** 
 Product.init(
 	{
 		id: {
@@ -53,6 +108,7 @@ Product.init(
 		modelName: "product",
 	}
 );
+*/
 
 // Product.hasOne(Category);
 module.exports = Product;
